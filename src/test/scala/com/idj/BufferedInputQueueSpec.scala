4d18293dfca0bda576da8978c1500acc9c20dd24
idj97 <ivkovicdjordje1997@gmail.com>
2023-11-18 23:39:46 +0100

    Add comments

--- src/test/scala/com/idj/BufferedInputQueueSpec.scala
+++ src/test/scala/com/idj/BufferedInputQueueSpec.scala
@@ -14,17 +14,27 @@ class BufferedInputQueueSpec extends AnyWordSpec {
   // that behavior of whole system is correct
   "test" in {
     val itemSource = new FakeUnorderedItemSource(120)
-    val conf = BufferedInputQueueConfig("itTest1", 50, 10, 2)
+    val conf = BufferedInputQueueConfig(
+      name = "itTest1",
+      maxQueueSize = 50,
+      maxBatchSize = 10,
+      maxConcurrency = 2
+    )
     val bufInQueue = BufferedInputQueue.create(itemSource, conf, Context.Simple.global)
     bufInQueue.start()
-
     val itemsAccumulator = mutable.Set[Int]()
+
+    // Wait 100ms and try to get 60 items from queue
+    // because maxQueueSize is 50 we will get 50
+    // check if items were seen before to make sure that
+    // buffered queue is not delivering duplicates
     Thread.sleep(100)
     val items1 = bufInQueue.get(60)
     assert(items1.length == 50)
     items1.foreach(i => assert(!itemsAccumulator.contains(i)))
     itemsAccumulator.addAll(items1)
 
+    // repeat for the next 50 items
     Thread.sleep(100)
     // make one of the get calls using async API
     val items2 = Await.result(bufInQueue.getAsync(60), Duration.Inf)
@@ -32,16 +42,19 @@ class BufferedInputQueueSpec extends AnyWordSpec {
     items2.foreach(i => assert(!itemsAccumulator.contains(i)))
     itemsAccumulator.addAll(items2)
 
+    // repeat for the last 20 items
     Thread.sleep(100)
     val items3 = bufInQueue.get(60)
     assert(items3.length == 20)
     items3.foreach(i => assert(!itemsAccumulator.contains(i)))
     itemsAccumulator.addAll(items3)
 
+    // at this point queue is empty
     Thread.sleep(100)
     val items4 = bufInQueue.get(600)
     assert(items4.isEmpty)
 
+    // shutdown queue
     bufInQueue.stop()
   }
 }
